ed0892796e15562e0b7f74e4662431c52a483bb6
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-02-26 02:28:16 +0700

    remove example without compose

--- example/without_composer/projects/site/settings/inertia.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    // All frontend related stuff go in here. It will be built using Vite
-    'resourcePath' => __DIR__ . '/../../../resources/',
-
-    // This is where you serve all static content. Beware of vite base path. 
-    'distPath' => __DIR__ . '/../../../js/',
-];
